c3a7c0bc7dd3b066280b46dc82e1bdbe31b5319c
Allan Medeiros <medeiros@codata.pb.gov.br>
2025-04-09 12:01:47 -0300

    Add Docker support and enable env var obfuscation

--- assincronismo/lib/config/env.dart
+++ assincronismo/lib/config/env.dart
@@ -4,18 +4,18 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'GITHUB_API_KEY')
-  static const String githubApiKey = _Env.githubApiKey;
+  @EnviedField(varName: 'GITHUB_API_KEY', obfuscate: true)
+  static String githubApiKey = _Env.githubApiKey;
 
-  @EnviedField(varName: 'GITHUB_AUTH_TOKEN')
-  static const String githubAuthToken = _Env.githubAuthToken;
+  @EnviedField(varName: 'GITHUB_AUTH_TOKEN', obfuscate: true)
+  static String githubAuthToken = _Env.githubAuthToken;
 
-  @EnviedField(varName: 'GITHUB_GIST_URL')
-  static const String githubGistUrl = _Env.githubGistUrl;
+  @EnviedField(varName: 'GITHUB_GIST_URL', obfuscate: true)
+  static String githubGistUrl = _Env.githubGistUrl;
 
-  @EnviedField(varName: 'CPF_URL')
-  static const String cpfUrl = _Env.cpfUrl;
+  @EnviedField(varName: 'CPF_URL', obfuscate: true)
+  static String cpfUrl = _Env.cpfUrl;
 
-  @EnviedField(varName: 'ISIS_TOKEN')
-  static const String isisToken = _Env.isisToken;
+  @EnviedField(varName: 'ISIS_TOKEN', obfuscate: true)
+  static String isisToken = _Env.isisToken;
 }
