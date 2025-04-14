624b2e91779fa0492454ee1987a12c77375b6ea4
Allan Medeiros <medeiros@codata.pb.gov.br>
2025-04-14 12:02:16 -0300

    Add images and improve login screen layout with background and logo

--- flutter_banco_douro/lib/ui/login_screen.dart
+++ flutter_banco_douro/lib/ui/login_screen.dart
@@ -6,39 +6,58 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Text(
-              'Sistema de Gestão de Contas',
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+      body: Stack(
+        children: [
+          Image.asset('assets/images/banner.png'),
+          Align(
+            child: Image.asset('assets/images/stars.png'),
+            alignment: Alignment.bottomLeft,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 120),
+                Image.asset('assets/images/logo.png', width: 120),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 32),
+                    const Text(
+                      'Sistema de Gestão de Contas',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 32),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Email'),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Senha'),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 32),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Color(0xFFFFA902)),
+                  ),
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 32),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {},
-              style: const ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Color(0xFFFFA902)),
-              ),
-              child: const Text(
-                'Entrar',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
