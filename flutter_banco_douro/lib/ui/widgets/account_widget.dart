af94b739632c01e15afcfacdc1aac7b65afcfda3
Allan Medeiros <medeiros.allan@gmail.com>
2025-04-14 22:05:58 -0300

    Add account widget and implement home screen layout with account details

---
+++ flutter_banco_douro/lib/ui/widgets/account_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/models/account.dart';
+import 'package:flutter_banco_douro/ui/styles/colors.dart';
+
+class AccountWidget extends StatelessWidget {
+  final Account account;
+  const AccountWidget({super.key, required this.account});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 128,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppColors.lightOrange,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("${account.lastName.toUpperCase()}, ${account.name}", style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),),
+              Text("ID: ${account.id}", style: const TextStyle(
+                fontSize: 14,
+              ),),
+              Text("Saldo: R\$ ${account.balance.toStringAsFixed(2)}", style: const TextStyle(
+                fontSize: 14,
+              ),),
+              Text("Tipo: ${account.accountType?.name} (Taxa: ${account.accountType?.tax}%)", style: const TextStyle(
+                fontSize: 14,
+              ),),
+            ],
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        ],
+      ),
+    );
+  }
+}
